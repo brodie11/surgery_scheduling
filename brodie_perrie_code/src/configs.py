741da72461dabc24fdfc84e1eabd28a0e9bc220b
pmac178 <111023996+pmac178@users.noreply.github.com>
2024-09-20 18:14:59 +1200

    output location reverted

--- brodie_perrie_code/src/configs.py
+++ brodie_perrie_code/src/configs.py
@@ -5,8 +5,8 @@ this_path = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE_DIR = os.path.abspath(os.path.join(this_path, os.pardir, 'data'))
 OUTPUT_DIR = os.path.abspath(os.path.join(this_path, os.pardir, 'output'))
-# OUTPUT_DB_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, 'databases'))
-OUTPUT_DB_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, 'perrie_experiments'))
+OUTPUT_DB_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, 'databases'))
+# OUTPUT_DB_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, 'perrie_experiments'))
 OUTPUT_DB_DIR_TEST = os.path.abspath(os.path.join(OUTPUT_DIR, 'databaseTests'))
 OUTPUT_FIG_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, 'figures'))
 
